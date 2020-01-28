3d62c51ed742b9ee27e052d5194aaac93fbd6772
William Lennartsson <william.j.lennartsson@gmail.com>
2020-01-28 11:49:52 +0100

    comments

--- userDefaultsDemo/ViewController.m
+++ userDefaultsDemo/ViewController.m
@@ -16,8 +16,43 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    // [self saveState];
+    [self loadState];
 }
 
+// For saving state
+- (void) saveState {
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    // Define state
+    NSDictionary *state = [self convertSelfToDictionary];
+    // Set state in defaults dictionary
+    [defaults setObject:state forKey:@"state"];
+    // Commit the changes
+    [defaults synchronize];
+    NSLog(@"Saved state : %@", state);
+}
+
+// Loading state
+- (void) loadState {
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    // Get state object from defaults.
+    NSDictionary *state = [defaults objectForKey:@"state"];
+    NSLog(@"Loaded state: %@", state);
+}
+
+
+// Helper functions to convert Class -> Dictionary and back
+//- (Person*) convertDictionaryToSelf: (NSDictionary*)d
+//- (NSDictionary*) convertPersonToDictionary: (Person*)p
+
+
+- (NSDictionary*) convertSelfToDictionary {
+    NSDictionary *state = @{
+        @"name": @"Bosse",
+        @"age": @12,
+        @"car": @"volvo"
+    };
+    return state;
+}
 
 @end
